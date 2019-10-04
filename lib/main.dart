29bc0972b09888cda2130d6433d86735d2e45148
Parag <paragspatil123@gmail.com>
2019-10-04 15:06:06 +0530

    Dice Assignment complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,70 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Roll the Dice'),
-          backgroundColor: Colors.red,
-        ),
-        body: Container(),
+      home: DicePage(),
       ),
-    ),
   );
 }
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  int  leftdicenumber = 5;
+  int rightdicenumber = 1;
+
+    void modifyrightdice() {
+      print('Right dicr clicked');
+      setState(() {
+              rightdicenumber = 1 + Random().nextInt(5);
+              leftdicenumber =  1 + Random().nextInt(5);
+      });
+    }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text('Roll the dice'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+                child: FlatButton(
+                  child: Image(
+                    image: AssetImage('assets/dice$leftdicenumber.png'),
+
+
+                  ),
+                  onPressed: () {
+                    modifyrightdice();
+                  },
+                )
+            ),
+            Expanded(
+              child: FlatButton(
+                  child: Image(
+                    image: AssetImage('assets/dice$rightdicenumber.png'),
+
+                  ),
+                  onPressed: (){
+                           modifyrightdice();
+                  }
+
+              ),
+            )
+          ],
+
+        ),
+      ),
+
+    );
+  }
+}
+
+
